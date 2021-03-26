50e314e40b45a2982e8d80f790ad6ae68de34161
darknessxk <me@darknessxk.com>
2021-03-26 19:28:59 -0300

    Updated script, changed command to password:hash instead of hasher

--- scripts/hashPassword.ts
+++ scripts/hashPassword.ts
@@ -1,5 +1,9 @@
 import { passwordHash } from '../src/utils';
 
-const password = process.argv[1] || '1234';
+const password = process.argv[0].endsWith('ts-node') ? process.argv[2] : process.argv[1];
 
-console.log(passwordHash(password), ':Hashed <--> Password: ', password);
+if (password === undefined) {
+    console.log('Password Hashing Script\nUsage: (npm run|yarn) password:hash 1234');
+} else {
+    console.log('Password Hashing Script\n\tHashed ->', passwordHash(password));
+}
