97b39b2b515c8394cb888edcd8423876abe52c7a
darknessxk <me@darknessxk.com>
2021-03-26 17:18:44 -0300

    Added migrations for type and user

---
+++ src/database/migration/1616788233592-add-type-table.ts
@@ -0,0 +1,33 @@
+import { MigrationInterface, QueryRunner, Table, TableIndex, TableColumn } from 'typeorm';
+
+export class addTypeTable1616788233592 implements MigrationInterface {
+    tableName: string = 'type';
+
+    public async up (queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: this.tableName,
+            columns: [
+                {
+                    name: 'id',
+                    type: 'varchar',
+                    isPrimary: true,
+                    default: 'UUID()'
+                },
+                {
+                    name: 'description',
+                    type: 'varchar',
+                    isNullable: false
+                }
+            ]
+        }));
+
+        await queryRunner.createIndex(this.tableName, new TableIndex({
+            name: 'IDX_TYPE_ID',
+            columnNames: ['id']
+        }));
+    }
+
+    public async down (queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable(this.tableName, true, true, true);
+    }
+}
---
+++ src/database/migration/1616788481233-add-user-table.ts
@@ -0,0 +1,57 @@
+import { MigrationInterface, QueryRunner, Table, TableIndex, TableColumn, TableForeignKey } from 'typeorm';
+
+export class addUserTable1616788481233 implements MigrationInterface {
+    tableName: string = 'users';
+
+    public async up (queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: this.tableName,
+            columns: [
+                {
+                    name: 'id',
+                    type: 'varchar',
+                    isPrimary: true,
+                    default: 'UUID()'
+                },
+                {
+                    name: 'email',
+                    type: 'varchar',
+                    isNullable: false,
+                    isUnique: true
+                },
+                {
+                    name: 'password',
+                    type: 'varchar',
+                    isNullable: false
+                },
+                {
+                    name: 'status',
+                    type: 'bool',
+                    isNullable: false,
+                    default: true
+                },
+                {
+                    name: 'typeId',
+                    type: 'varchar',
+                    isNullable: false
+                }
+            ]
+        }));
+
+        await queryRunner.createIndex(this.tableName, new TableIndex({
+            name: 'IDX_TYPE_ID',
+            columnNames: ['id']
+        }));
+
+        await queryRunner.createForeignKey(this.tableName, new TableForeignKey({
+            columnNames: ['typeId'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'type',
+            onDelete: 'CASCADE'
+        }));
+    }
+
+    public async down (queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable(this.tableName);
+    }
+}
