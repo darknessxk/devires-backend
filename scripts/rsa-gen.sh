0a3d0050067926141f949c1b54634be4b1304a59
darknessxk <me@darknessxk.com>
2021-03-26 13:34:04 -0300

    Added rsa generation script

---
+++ scripts/rsa-gen.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+KEYS_PATH="$PWD/../keys"
+PVK="jwt.pvk"
+PBK="jwt.pbk"
+
+if [ ! -d $KEYS_PATH ]; then
+    mkdir $KEYS_PATH
+fi
+
+ssh-keygen -t rsa -b 4096 -m PEM -f "$KEYS_PATH/$PVK"
+openssl rsa -in "$KEYS_PATH/$PVK" -pubout -outform PEM -out "$KEYS_PATH/$PBK"
+
