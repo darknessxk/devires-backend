4f5446e3accb1f993a266964133ff35f575f50ec
darknessxk <me@darknessxk.com>
2021-03-26 14:45:03 -0300

    Testing env, removed jwk parsing from getkey now just returns buffer instead of parsed item

--- tests/getJwtKey.test.ts
+++ tests/getJwtKey.test.ts
@@ -1,5 +1,17 @@
 import { getJwtKey } from '../utils';
 
-test('getJwtKey without env variable', () => {
+test('getJwtKey without env variable JWT_PBK', () => {
     expect(() => getJwtKey('PBK')).toThrow('JWT_PBK environment is missing');
+});
+
+test('getJwtKey with env variable JWT_PBK', () => {
+    expect(() => getJwtKey('PBK')).toBeDefined();
+});
+
+test('getJwtKey without env variable JWT_PVK', () => {
+    expect(() => getJwtKey('PVK')).toThrow('JWT_PVK environment is missing');
+});
+
+test('getJwtKey with env variable JWT_PVK', () => {
+    expect(() => getJwtKey('PVK')).toBeDefined();
 });
--- utils/getJwtKey.ts
+++ utils/getJwtKey.ts
@@ -1,13 +1,12 @@
-import { JWK, fromKeyLike } from 'jose/jwk/from_key_like';
 import fs from 'fs';
 
-export const getJwtKey = async (type: 'PBK' | 'PVK'): Promise<JWK> => {
+type JwtKeys = 'PBK' | 'PVK';
+
+export const getJwtKey = async (type: JwtKeys): Promise<Buffer> => {
     const KEY = process.env[`JWT_${type}`];
 
     if (KEY) {
-        const fileBuffer = fs.readFileSync(KEY);
-
-        return fromKeyLike(fileBuffer);
+        return fs.readFileSync(KEY);
     } else {
         throw new Error(`JWT_${type} environment is missing`);
     }
