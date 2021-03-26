6cc501b2a96ff76d8338ccf55250de6ce58281ca
darknessxk <me@darknessxk.com>
2021-03-26 17:49:17 -0300

    Added body parser

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'reflect-metadata';
 import routes from './routes';
 import path from 'path';
+import bodyParser from 'body-parser';
 
 /**
  * Application entrypoint
@@ -10,6 +11,9 @@ const main = async () => {
     const app = express();
     const { PORT } = process.env;
     const httpPort = PORT || 1030;
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded());
 
     app.use('/docs', express.static(path.join(__dirname, '../docs')));
     app.use('/', routes);
--- src/routes/api/login/index.ts
+++ src/routes/api/login/index.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const router = Router();
 
 router.post('/', (req, res) => {
-    res.send('...');
+
 });
 
 export default router;
