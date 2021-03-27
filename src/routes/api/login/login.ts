85a53ab484a2a5c197a2fc44f94b37f195189b90
darknessxk <me@darknessxk.com>
2021-03-27 17:29:41 -0300

    Login Api Route

--- src/routes/api/login/login.ts
+++ src/routes/api/login/login.ts
@@ -1,18 +1,15 @@
-import { User as DatabaseUser } from '../../../database/models/User';
+import { User as DbUser } from '../../../database/models/User';
 import { User } from '../../../types';
 import connection from '../../../database/connectionHandler';
 import { passwordHash } from '../../../utils';
 
-export const login = async (email: string, password: string): Promise<User | false> => {
+export const login = async (email: string, pass: string): Promise<User | false> => {
     const conn = await connection.get();
-    const userRepository = conn.getRepository(DatabaseUser);
-    const hashed = passwordHash(password);
+    const userRepository = conn.getRepository(DbUser);
+    const password = passwordHash(pass);
 
     const user = await userRepository.findOne({
-        where: {
-            email,
-            password: hashed
-        }
+        where: { email, password }
     });
 
     return user as User || false;
