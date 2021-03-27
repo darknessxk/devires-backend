18c316fb6c15103e47d2035e09b0a090cf75729e
darknessxk <me@darknessxk.com>
2021-03-27 17:27:47 -0300

    Removed Typeorm Seeding package and files

--- src/routes/api/user/fetchInformation.ts
+++
@@ -1,8 +0,0 @@
-import { User } from '../../../types';
-import connection from '../../../database/connectionHandler';
-
-export const fetchInformation = async (token: string, id?: string): Promise<User | false> => {
-    const conn = await connection.get();
-
-    return false;
-};
