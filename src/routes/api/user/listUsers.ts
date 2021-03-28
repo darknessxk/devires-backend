46d79ff43019820078df6684c1c1358b7fe344f1
darknessxk <me@darknessxk.com>
2021-03-28 18:50:59 -0300

    Support for arguments when fetching information

--- src/routes/api/user/listUsers.ts
+++ src/routes/api/user/listUsers.ts
@@ -1,12 +1,18 @@
 import { User as DbUser } from '../../../database/models/User';
 import { User } from '../../../types';
 import connection from '../../../database/connectionHandler';
+import { QueryDeepPartialEntity } from 'typeorm/query-builder/QueryPartialEntity';
 
-export const listUsers = async (): Promise<User[]> => {
+export const listUsers = async (data?: QueryDeepPartialEntity<DbUser>): Promise<User[]> => {
     const conn = await connection.get();
     const repo = conn.getRepository(DbUser);
 
-    const users = await repo.find();
+    let users: DbUser[];
+    if (data) {
+        users = await repo.find({ where: data, relations: ['type'] });
+    } else {
+        users = await repo.find({ relations: ['type'] });
+    }
 
     return users && users.map(({ type, status, email, id }) => ({ id, status, email, type }));
 };
