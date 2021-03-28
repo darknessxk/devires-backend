09f8370e1f35242f5ac1fd9b772cdb94273c0a6a
darknessxk <me@darknessxk.com>
2021-03-28 16:17:08 -0300

    Update routers

--- src/routes/api/login/router.ts
+++ src/routes/api/login/router.ts
@@ -1,8 +1,28 @@
 import { Router as ERouter } from 'express';
+import { login } from './login';
+import { IsEmail } from 'class-validator';
+import { signJwt } from '../../../utils';
 const router = ERouter();
 
-router.post('/', (req, res) => {
-    res.send('!');
+router.post('/', async (req, res) => {
+    const {
+        body: {
+            email, password
+        }
+    } = req;
+
+    if (!IsEmail(email)) {
+        res.status(400).end();
+    } else {
+        const loginResult = await login(email, password);
+
+        if (!loginResult) {
+            res.status(401).end();
+        } else {
+            const token = signJwt(loginResult);
+            res.status(200).send({ token });
+        }
+    }
 });
 
 export const Router = router;
--- src/routes/api/user/router.ts
+++ src/routes/api/user/router.ts
@@ -1,9 +1,30 @@
 import { Router as ERouter } from 'express';
+import * as UserApi from './';
 
 const router = ERouter();
 
-router.post('/', (req, res) => {
-    res.send('!');
+router.get('/', async (req, res) => {
+    res.send(await UserApi.listUsers());
+});
+
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    res.send(await UserApi.getUserById(id));
+});
+
+router.post('/', async (req, res) => {
+    const { email, status, password, type } = req.body;
+    res.send(await UserApi.createUser({ email, status, password, type }));
+});
+
+router.delete('/', async (req, res) => {
+    const { id } = req.body;
+    res.send(await UserApi.deleteUser(id));
+});
+
+router.patch('/', async (req, res) => {
+    const { id, email, status, password, type } = req.body;
+    res.send(await UserApi.updateUser(id, { email, status, password, type }));
 });
 
 export const Router = router;
