e81bccdb3e7700bd9f652d2623118903cf147393
darknessxk <me@darknessxk.com>
2021-03-28 16:25:21 -0300

    Now server creates a instance instead of running inside the app entrypoint

---
+++ src/server/index.ts
@@ -0,0 +1,18 @@
+import 'reflect-metadata';
+import http from 'http';
+import express from 'express';
+import bodyParser from 'body-parser';
+import path from 'path';
+import routes from '../routes';
+
+export const Server = (): http.Server => {
+    const app = express();
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.use('/docs', express.static(path.join(__dirname, '../docs')));
+    app.use('/', routes);
+
+    return http.createServer({}, app);
+};
