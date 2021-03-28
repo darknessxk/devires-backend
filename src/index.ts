1f96d1d1e6451c0f444e006d111d764110642a5f
darknessxk <me@darknessxk.com>
2021-03-28 16:24:38 -0300

    Updated app main to be less boilerplated

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,10 @@
-import express from 'express';
-import 'reflect-metadata';
-import routes from './routes';
-import path from 'path';
-import bodyParser from 'body-parser';
+import { Server } from './server';
 
-const main = async () => {
-    const app = express();
-    const { PORT } = process.env;
-    const httpPort = PORT || 1030;
+const { env: { PORT } } = process;
+const httpPort = PORT || 1030;
 
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded());
-
-    app.use('/docs', express.static(path.join(__dirname, '../docs')));
-    app.use('/', routes);
-
-    app.listen(httpPort, () => console.log(`Listening to port ${httpPort}`));
-};
-
-main()
-    .then(r => console.log.bind(this, '[Backend App] Completed tasks...', r))
-    .catch(e => console.error.bind(this, '[Backend App] Error', e));
+Server()
+    .on('listening', () => {
+        console.log('Listening to port:', httpPort);
+    })
+    .listen(httpPort);
