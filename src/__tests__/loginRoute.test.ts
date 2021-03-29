e427610f13c62b44b5c3d23958b500e1c249dead
darknessxk <me@darknessxk.com>
2021-03-29 02:51:34 -0300

    Added new test cases into login route test suite

--- src/__tests__/loginRoute.test.ts
+++ src/__tests__/loginRoute.test.ts
@@ -18,7 +18,35 @@ describe('Api /login Route', () => {
     });
     afterAll(async () => dbClose());
 
-    test('test login', () => {
+    test('no email', () => {
+        return request(app)
+            .post('/api/login')
+            .send({ ...loginInfo, email: undefined })
+            .expect(400);
+    });
+
+    test('no password', () => {
+        return request(app)
+            .post('/api/login')
+            .send({ ...loginInfo, password: undefined })
+            .expect(400);
+    });
+
+    test('invalid email', () => {
+        return request(app)
+            .post('/api/login')
+            .send({ ...loginInfo, email: 'INVALID' })
+            .expect(400);
+    });
+
+    test('invalid password', () => {
+        return request(app)
+            .post('/api/login')
+            .send({ ...loginInfo, password: 'INVALID' })
+            .expect(401);
+    });
+
+    test('valid login', () => {
         return request(app)
             .post('/api/login')
             .send(loginInfo)
