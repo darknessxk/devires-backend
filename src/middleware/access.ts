9cb84a938a4d362748eed388d8aa6c8c848d04ac
darknessxk <me@darknessxk.com>
2021-03-29 02:37:14 -0300

    Better deconstruct pattern usage reduced if usage

--- src/middleware/access.ts
+++ src/middleware/access.ts
@@ -1,16 +1,18 @@
-import { Request, Response, NextFunction } from 'express';
+import { Response, NextFunction, Request } from 'express';
 import { checkAccess } from '../utils';
 
 export const access = (req: Request, res: Response, next: NextFunction): void => {
-    const { user } = req;
-
-    if (user) {
-        if (checkAccess(user.type.description)) {
-            next();
-        } else {
-            res.status(401).send({ msg: 'Access denied' });
+    const {
+        user: {
+            type: {
+                description
+            }
         }
+    } = req;
+
+    if (checkAccess(description)) {
+        next();
     } else {
-        res.status(400).send({ msg: 'Invalid token' });
+        res.status(401).send({ msg: 'Access denied' });
     }
 };
