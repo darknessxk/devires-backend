8449a7ca79e32e125ddfc27c0a2c5fc2213d2fc6
darknessxk <me@darknessxk.com>
2021-03-29 02:51:43 -0300

    Removed dbInit

--- src/routes/api/login/login.ts
+++ src/routes/api/login/login.ts
@@ -1,11 +1,9 @@
 import { User as DbUser } from '../../../database/models/User';
 import { User } from '../../../types';
-import { initialize as dbInit } from '../../../database/connectionHandler';
 import { passwordHash } from '../../../utils';
 import { getRepository } from 'typeorm';
 
 export const login = async (email: string, pass: string): Promise<User | false> => {
-    await dbInit();
     const userRepository = getRepository(DbUser);
     const password = passwordHash(pass);
 
